75ddf00f735a7026381485149d364853bfcf3d47
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-23 20:25:48 +0300

    Service description

--- store/models.py
+++ store/models.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class Service(models.Model):
         service_name = models.CharField(max_length=200, null= True)
+        service_description = models.CharField(max_length=500, null= True)
+        
