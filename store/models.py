8f7cd6c65cad2ee1dcda54fa4ee4f2fcdca685fe
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-23 20:26:29 +0300

    Service images

--- store/models.py
+++ store/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Service(models.Model):
         service_name = models.CharField(max_length=200, null= True)
         service_description = models.CharField(max_length=500, null= True)
-        
+        service_image = models.URLField(null=True)
