b157c8348f781b0264ff7748ce72ea34da374511
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-23 20:28:01 +0300

    __str__()

--- store/models.py
+++ store/models.py
@@ -4,3 +4,6 @@ class Service(models.Model):
         service_name = models.CharField(max_length=200, null= True)
         service_description = models.CharField(max_length=500, null= True)
         service_image = models.URLField(null=True)
+
+        def __str__(self):
+                return self.service_name
