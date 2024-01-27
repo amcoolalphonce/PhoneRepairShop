dbbb74e3ef6811622fd4db2adf4c08e5071b7b64
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-27 20:34:50 +0300

    about view

--- store/views.py
+++ store/views.py
@@ -5,3 +5,6 @@ def index(request):
         services = Service.objects.all()
         context = {'services' : services}
         return render(request, 'store/index.html', context)
+
+def about(request):
+        return render(request, 'store/about.html')
